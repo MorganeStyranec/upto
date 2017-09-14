c08d38cc520b399bd9b54900b8aa6b1375c60cee
MorganeStyranec <morgane.styranec@gmail.com>
2017-09-14 11:16:05 +0200

    commentaires

---
+++ 2ème Exercice/Exos JS/fichier.js
@@ -0,0 +1,10 @@
+alert("Hello World !");
+
+/*var age=prompt("Quel est votre âge ?");
+if (age<=17)
+{console.log("Vous n'êtes pas encore majeur.");}
+else if (age=<18 || <49)
+{console.log("Vous êtes majeur mais pas encore senior.");}
+else if (age<50 || <59)
+{console.log("Vous êtes senior mais pas encore retraité.");}
+else {console.log("Vous êtes retraité, profitez de votre temps libre.");}
